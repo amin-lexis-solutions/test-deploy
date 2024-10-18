a7f02f9c679c8118c9b3d6b953c5fb0b5e024ca1
Amin Mbarki <amin@lexis.solutions>
2024-10-18 16:16:03 +0100

    Update .gitignore to ignore package-lock.json

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   output: 'standalone',
   env: {
-    API_URL: process.env.API_URL || 'http://localhost:3000',
-    API_SECRET: process.env.API_SECRET || 'secret',
+    API_URL: process.env.API_URL ,
+    API_SECRET: process.env.API_SECRET,
   },
 }
 
